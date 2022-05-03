dcfc7622d355e6799a89a561e7767cfd6ecda4e1
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-05-03 08:25:21 -0600

    first comment

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
 
+/// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
